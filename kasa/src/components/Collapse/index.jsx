6379eedd0c2b33f06cd532d3d47532c026679fd2
SyDyJo <gadyamb.re@gmail.com>
2025-03-20 13:43:18 +0100

    update: modification de l'attribut style par styled

--- kasa/src/components/Collapse/index.jsx
+++ kasa/src/components/Collapse/index.jsx
@@ -4,8 +4,14 @@ import '../../utils/style/Collapse.scss'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronUp } from '@fortawesome/free-solid-svg-icons'
+import styled from 'styled-components'
 
 function Collapse({ title, content }) {
+  const StyledIcon = styled(FontAwesomeIcon)`
+    cursor: pointer;
+    transition: transform 1s ease;
+    transform: rotate(${(props) => props.rotation}deg);
+  `
   const [isOpen, setIsOpen] = useState(false)
 
   const [rotation, setRotation] = useState(0)
@@ -19,14 +25,10 @@ function Collapse({ title, content }) {
     <div className="collapse">
       <summary className="collapse__summary">
         <h3 className="collapse__title">{title}</h3>
-        <FontAwesomeIcon
+        <StyledIcon
           icon={faChevronUp}
+          rotation={rotation}
           onClick={handleClick}
-          style={{
-            cursor: 'pointer',
-            transition: 'transform 0.3s ease',
-            transform: `rotate(${rotation}deg)`,
-          }}
         />
       </summary>
       <p className={`collapse__content${isOpen ? '--open' : ''}`}>{content}</p>
