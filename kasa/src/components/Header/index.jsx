39c20473642a979b5fd5652a3767907c8eb72e16
SyDyJo <gadyamb.re@gmail.com>
2025-03-27 14:13:42 +0100

    feat: Implémenter le Header

--- kasa/src/components/Header/index.jsx
+++ kasa/src/components/Header/index.jsx
@@ -1,11 +1,13 @@
 import { Link } from 'react-router'
 import logo from '../../assets/logo.png'
 
+import '../../utils/style/Header.scss'
+
 function Header() {
   return (
-    <div>
+    <div className="header">
       <img className="logo" alt="logo" src={logo} />
-      <nav>
+      <nav className="header__nav">
         <Link to="/">Accueil</Link>
         <Link to="/About">A propos</Link>
       </nav>
